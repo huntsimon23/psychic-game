f7a31210684108ef47dece8f26c1f8d8b5b88e89
huntsimon23 <hunt.simon23@gmail.com>
2019-03-14 16:30:48 -0400

    js working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]; 
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z",]; 
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessText = [];
 
 var userGuessText = document.getElementById("userguesses-text");
 var winsText = document.getElementById("wins-text");
@@ -19,37 +20,26 @@ document.onkeyup = function(event) {
         winsText.textContent = wins;
         guessesLeft = 9;
         alert("You win, computer chose " + computerGuess + " and so did you!");
-        
+        userGuessText.textContent = [];
+        guessText = [];
 }   
     else {
-    (userGuessText.textContent = userGuess);
     guessesLeft = (guessesLeft - 1);     
     guessesLeftText.textContent = guessesLeft;
-    
-    function addLetters() {
-        var node = document.createElement("span");
-        var textnode = document.createTextNode(userGuess);
-        node.appendChild(textnode);
-        document.getElementById("userguesses-text").appendChild(node);}
+    guessText.push(userGuess);
+    userGuessText.textContent = guessText; 
 }
-    if (guessesLeft < 1) {
+    if (guessesLeft === 0) {
     losses++;
     lossesText.textContent = losses;
     guessesLeft = 9;
     alert("Sorry, out of tries.  Give it another shot.")
+    guessesLeftText.textContent = guessesLeft;
+    userGuessText.textContent = [];
+    guessText = [];
 }
+    else {
+        
 }
 
-// 6. Guesses Left: (# of guesses left. This will update)
-
-// 7. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-// 8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-// 9. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
-// function addLetters() {
-//     var node = document.createElement("span");
-//     var textnode = document.createTextNode(userGuess.key);
-//     node.appendChild(textnode);
-//     document.getElementById("myuserguesses-text").appendChild(node);}
+}
